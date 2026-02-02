fb69ca6a68fc8f61fee9265da2283e627ff7a0e6
ye-seob <jakalob103@naver.com>
2026-02-02 21:29:08 +0900

    BaseLine 코드 작성

---
+++ src/main/java/com/concurrency/springconcurrency/SpringConcurrencyApplication.java
@@ -0,0 +1,13 @@
+package com.concurrency.springconcurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConcurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringConcurrencyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/concurrency/springconcurrency/SpringConcurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concurrency.springconcurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConcurrencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
