0486043d598f6f77774c05819570abf93a76c3b9
ye-seob <jakalob103@naver.com>
2026-02-03 12:01:15 +0900

    @Synchronized 적용

--- src/main/java/com/concurrency/springconcurrency/EnrollmentService.java
+++ src/main/java/com/concurrency/springconcurrency/EnrollmentService.java
@@ -1,5 +1,6 @@
 package com.concurrency.springconcurrency;
 
+import lombok.Synchronized;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,6 +9,7 @@ public class EnrollmentService {
     private int capacity = 30;
     private int enrolled = 0;
 
+    @Synchronized
     public void enroll() throws InterruptedException {
         Thread.sleep(50); // 스레드들이 동일한 시점에 임계 구간에 진입하도록 유도하기 위한 장치
 
